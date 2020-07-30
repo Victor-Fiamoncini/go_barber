77dcce5eef8376f8d53be0975d17f7f55143b730
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-07-30 19:56:34 -0300

    first commit

---
+++ server/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2020: true,
+		jest: true,
+	},
+	extends: 'eslint:recommended',
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint'],
+	parserOptions: {
+		ecmaVersion: 11,
+		sourceType: 'module',
+	},
+	globals: {
+		Atomics: 'readonly',
+		SharedArrayBuffer: 'readonly',
+		process: true,
+		module: true,
+		export: true,
+		__dirname: true,
+		require: true,
+	},
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'no-unused-vars': 'off',
+		'no-undef': 'off',
+	},
+}
