222147c2b1a08a94aa502e383ae500f04d96b17e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-07-30 21:32:09 -0300

    feat: appointments model

---
+++ server/src/routes/appointments.routes.ts
@@ -0,0 +1,32 @@
+import { Router } from 'express'
+import { startOfHour, parseISO, isEqual } from 'date-fns'
+
+import Appointment from '../app/models/Appointment'
+
+const appointmentsRouter = Router()
+
+const appointments: Appointment[] = []
+
+appointmentsRouter.post('/', (request, response) => {
+	const { provider, date } = request.body
+
+	const parsedDate = startOfHour(parseISO(date))
+
+	const findAppointmentInSameDate = appointments.find(appointment =>
+		isEqual(parsedDate, appointment.date)
+	)
+
+	if (findAppointmentInSameDate) {
+		return response
+			.status(400)
+			.json({ message: 'This appointment is already booked' })
+	}
+
+	const appointment = new Appointment(provider, parsedDate)
+
+	appointments.push(appointment)
+
+	return response.status(201).json(appointment)
+})
+
+export default appointmentsRouter
