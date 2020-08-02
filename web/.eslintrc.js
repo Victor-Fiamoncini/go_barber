828392e59dc68e49e2497c3ebd593d0892c69a2b
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-01 23:19:53 -0300

    feat: Register style

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -4,23 +4,27 @@ module.exports = {
 		es2020: true,
 	},
 	extends: [
-		"eslint:recommended",
-		"plugin:react/recommended",
-		"plugin:@typescript-eslint/recommended",
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
 	],
-	parser: "@typescript-eslint/parser",
+	parser: '@typescript-eslint/parser',
+	globals: {
+		module: true,
+	}
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
 		},
 		ecmaVersion: 11,
-		sourceType: "module",
+		sourceType: 'module',
 	},
-	plugins: ["react", "@typescript-eslint"],
+	plugins: ['react', '@typescript-eslint'],
 	rules: {
-		indent: ["error", "tab"],
-		"linebreak-style": ["error", "unix"],
-		quotes: ["error", "single"],
-		semi: ["error", "never"],
+		indent: ['error', 'tab'],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'react/prop-types': 'off',
 	},
-};
+}
