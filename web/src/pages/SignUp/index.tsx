e41eb0cec40875e86b44edc6e79e36e3edad1ee3
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-02 00:55:01 -0300

    feat: Unform install

--- web/src/pages/SignUp/index.tsx
+++ web/src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Form } from '@unform/web'
 
 import { Container, Content, Background } from './styles'
 
@@ -9,18 +10,20 @@ import Input from '../../components/Input'
 import Button from '../../components/Button'
 
 const SignUp: React.FC = () => {
+	function handleSubmit(data: object) {}
+
 	return (
 		<Container>
 			<Background />
 			<Content>
 				<img src={logo} alt="GoBarber" />
-				<form>
+				<Form onSubmit={handleSubmit}>
 					<h1>Faça seu cadastro</h1>
 					<Input name="name" icon={FiUser} placeholder="Nome" />
 					<Input name="email" icon={FiMail} placeholder="E-mail" />
 					<Input name="password" icon={FiLock} placeholder="Senha" />
 					<Button type="submit">Cadastrar</Button>
-				</form>
+				</Form>
 				<a href="/login">
 					<FiArrowLeft />
 					Voltar para logon
