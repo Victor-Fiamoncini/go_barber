f87554add4442d0dfbeb1a579859f12d142b8af1
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-04 20:20:47 -0300

    feat: user auth unit tests

--- server/src/modules/users/services/AuthenticateUserService.spec.ts
+++ server/src/modules/users/services/AuthenticateUserService.spec.ts
@@ -34,4 +34,48 @@ describe('AuthenticateUser', () => {
 		expect(response).toHaveProperty('token')
 		expect(response.user).toEqual(user)
 	})
+
+	it('should not be able to authenticate with non existing user', async () => {
+		const fakeUsersRepository = new FakeUsersRepository()
+		const fakeHashProvider = new FakeHashProvider()
+
+		const authenticateUserService = new AuthenticateUserService(
+			fakeUsersRepository,
+			fakeHashProvider
+		)
+
+		expect(
+			authenticateUserService.execute({
+				email: 'johndoe@mail.com',
+				password: '123456',
+			})
+		).rejects.toBeInstanceOf(AppError)
+	})
+
+	it('should not be able to authenticate with wrong password', async () => {
+		const fakeUsersRepository = new FakeUsersRepository()
+		const fakeHashProvider = new FakeHashProvider()
+
+		const createUserService = new CreateUserService(
+			fakeUsersRepository,
+			fakeHashProvider
+		)
+		const authenticateUserService = new AuthenticateUserService(
+			fakeUsersRepository,
+			fakeHashProvider
+		)
+
+		await createUserService.execute({
+			name: 'John Doe',
+			email: 'johndoe@mail.com',
+			password: '123456',
+		})
+
+		expect(
+			authenticateUserService.execute({
+				email: 'johndoe@mail.com',
+				password: 'wrongPassword',
+			})
+		).rejects.toBeInstanceOf(AppError)
+	})
 })
