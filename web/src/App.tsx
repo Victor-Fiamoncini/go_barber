3e782486e6b2a2ceea0d4e937b09002a8807031c
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-16 21:47:19 -0300

    feat: toast container

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,6 +4,7 @@ import { AuthProvider } from './context/AuthContext'
 
 import GlobalStyle from './styles/global'
 
+import ToastContainer from './components/ToastContainer'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 
@@ -12,6 +13,7 @@ const App: React.FC = () => (
 		<AuthProvider>
 			<SignIn />
 		</AuthProvider>
+		<ToastContainer />
 		<GlobalStyle />
 	</>
 )
--- web/src/pages/SignIn/index.tsx
+++ web/src/pages/SignIn/index.tsx
@@ -43,8 +43,11 @@ const SignIn: React.FC = () => {
 
 				await signIn({ email, password })
 			} catch (err) {
-				const errors = getValidationErrors(err)
-				formRef.current?.setErrors(errors)
+				if (err instanceof Yup.ValidationError) {
+					const errors = getValidationErrors(err)
+
+					formRef.current?.setErrors(errors)
+				}
 			}
 		},
 		[signIn]
