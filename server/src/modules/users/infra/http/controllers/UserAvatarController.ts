cf4ed08032fd15d81e99443d12d0ed66b624b652
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-18 21:48:12 -0300

    feat: add class-transformer

--- server/src/modules/users/infra/http/controllers/UserAvatarController.ts
+++ server/src/modules/users/infra/http/controllers/UserAvatarController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express'
 import { container } from 'tsyringe'
+import { classToClass } from 'class-transformer'
 
 import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService'
 
@@ -10,9 +11,7 @@ class UsersAvatarController {
 			avatarFilename: request.file.filename,
 		})
 
-		delete user.password
-
-		return response.status(200).json(user)
+		return response.status(200).json(classToClass(user))
 	}
 }
 
