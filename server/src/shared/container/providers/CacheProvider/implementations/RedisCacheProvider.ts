23f23c991e2ade73c7210b4090dba2c1706bc64d
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-08-19 21:58:31 -0300

    feat: add cache to users list

--- server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
+++ server/src/shared/container/providers/CacheProvider/implementations/RedisCacheProvider.ts
@@ -11,17 +11,33 @@ class RedisCacheProvider implements ICacheProvider {
 		this.client = new Redis(cacheConfig.config.redis)
 	}
 
-	public async save(key: string, value: string) {
-		await this.client.set(key, value)
+	public async save(key: string, value: any) {
+		await this.client.set(key, JSON.stringify(value))
 	}
 
-	public async recover(key: string) {
+	public async recover<T>(key: string) {
 		const data = await this.client.get(key)
 
-		return data
+		if (!data) {
+			return null
+		}
+
+		const parsedData = JSON.parse(data) as T
+
+		return parsedData
 	}
 
 	public async invalidate(key: string) {}
+
+	public async invalidatePrefix(prefix: string) {
+		const keys = await this.client.keys(`${prefix}:*`)
+
+		const pipeline = this.client.pipeline()
+
+		keys.forEach(key => pipeline.del(key))
+
+		await pipeline.exec()
+	}
 }
 
 export default RedisCacheProvider
