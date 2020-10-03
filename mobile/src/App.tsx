5361210006ff6d930001f7351b16b592aa69a977
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2020-10-03 20:55:14 -0300

    chore: font families setup

--- mobile/src/App.tsx
+++ mobile/src/App.tsx
@@ -1,9 +1,33 @@
+import 'react-native-gesture-handler'
+
 import React from 'react'
+import { View } from 'react-native'
 import { registerRootComponent } from 'expo'
 import { StatusBar } from 'expo-status-bar'
+import { useFonts } from 'expo-font'
+import { NavigationContainer } from '@react-navigation/native'
+
+import Routes from './routes'
+
+import robotoSlabRegular from '../assets/fonts/RobotoSlab-Regular.ttf'
+import robotoSlabMedium from '../assets/fonts/RobotoSlab-Medium.ttf'
 
 const App: React.FC = () => {
-	return <StatusBar style="dark" />
+	const [isLoaded] = useFonts({
+		'RobotoSlab-Regular': robotoSlabRegular,
+		'RobotoSlab-Medium': robotoSlabMedium,
+	})
+
+	return (
+		<NavigationContainer>
+			<StatusBar style="light" backgroundColor="#312e38" />
+			{isLoaded && (
+				<View style={{ flex: 1, backgroundColor: '#312e38' }}>
+					<Routes />
+				</View>
+			)}
+		</NavigationContainer>
+	)
 }
 
 export default registerRootComponent(App)
---
+++ mobile/src/pages/SignIn/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View``
+
+export const Title = styled.Text`
+	font-size: 20px;
+	color: #f4ebe8;
+	font-family: 'RobotoSlab-Medium';
+`
